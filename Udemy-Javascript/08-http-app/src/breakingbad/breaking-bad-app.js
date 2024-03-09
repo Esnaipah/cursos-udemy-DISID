f99516318091bfc18773a6ff11e9cb3a2b1387c6
psalido <141011675+psalidodisid@users.noreply.github.com>
2024-03-09 10:43:08 +0100

    sync

--- Udemy-Javascript/08-http-app/src/breakingbad/breaking-bad-app.js
+++ Udemy-Javascript/08-http-app/src/breakingbad/breaking-bad-app.js
@@ -6,9 +6,8 @@ const fetchQuote = async () => {
     const res = await fetch('https://api.breakingbadquotes.xyz/v1/quotes/');
     const data = await res.json(); // para interpretar el body
 
-    console.log(data[0]);
+    return data[0];
 }
-
 /**
  * 
  * @param {HTMLDivElement} element 
@@ -18,7 +17,25 @@ export const BreakingbadApp = (element) => {
     document.querySelector('#app-title').innerHTML = 'Breaking Bad app';
     element.innerHTML = 'Loading...';
     fetchQuote();
-    element.innerHTML = 'Tenemos data';
 
-    
+    const quoteLabel = document.createElement('blockquote');
+    const authorLabel = document.createElement('h3');
+    const nextQuoteButton = document.createElement('button');
+    nextQuoteButton.innerText = 'Next Quote';
+
+    const renderQuote = (data) => {
+        quoteLabel.innerHTML = data.quote;
+        authorLabel.innerHTML = data.author;
+        element.replaceChildren(quoteLabel, authorLabel, nextQuoteButton);
+    }
+
+    nextQuoteButton.addEventListener('click', () => {
+        element.innerHTML = 'Loading...';
+        fetchQuote()
+            .then(renderQuote);
+    })
+
+    fetchQuote()
+        .then(renderQuote);
+
 }
